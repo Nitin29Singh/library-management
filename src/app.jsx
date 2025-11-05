e16e74889f47a4dc3950498b1e10143645248385
Nitin29Singh <nitin2004singhh29@gmail.com>
2025-11-05 22:17:20 +0530

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LibraryManager from "./components/LibraryManager";
+
+function App() {
+  return (
+    <div className="App">
+      <LibraryManager />
+    </div>
+  );
+}
+
+export default App;
